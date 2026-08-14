38414cf6d53af579fa1f55e5bf89824e74a361f8
Igor B Sousa <ibarbosadsousa@gmail.com>
2026-08-13 21:06:13 -0300

    atualizando páginas de admin

--- vida-plena/app/admin/usuarios/novo/page.tsx
+++ vida-plena/app/admin/usuarios/novo/page.tsx
@@ -3,12 +3,25 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { formatarCPF, limparCPF } from "@/app/lib/Formatters";
-import { cpfValido, emailValido } from "@/app/lib/Validation";
-// TO DO: dar interação a respostas "erradas", mostrando ao usuario que errou.
-// Adicionar um campo extra para opção como: cuidador, médico, nutricionista.
+import { cpfValido, emailValido, dataValida, crmValido, crnValido } from "@/app/lib/Validation";
+
+import Button from "@/app/components/Button";
+// TO DO: Cuidador precisa de que campo?
 export default function CadastroUsuarios() {
    const router = useRouter();
    const [cpf, setCpf] = useState("");
+   const [campoInvalido, showCampoInvalido] = useState({
+      cpf: false,
+      email: false,
+      data: false,
+      senha: false,
+      crm: false,
+      crn:false,
+   });
+   const [campoExtra, showCampoExtra] = useState({
+      crm: false,
+      crn: false,
+   });
 
    function modificaCPF(cpf: string) {
 
@@ -17,98 +30,136 @@ export default function CadastroUsuarios() {
       setCpf(cpf);
    }
 
+   function validaFormulario(event: React.FormEvent<HTMLFormElement>) {
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+      const dados = Object.fromEntries(formData.entries())
+
+      const cpf = limparCPF(String(dados.cpf))
+      const dataNascimento = String(dados.dataNascimento)
+
+      const emailInvalido = !emailValido(String(dados.email));
+      const cpfInvalido = !cpfValido(cpf);
+      const senhaInvalida = dados.senha !== dados.repetirSenha;
+      const dataInvalida = !dataValida(dataNascimento);
+
+      const crmInvalido = campoExtra.crm ? !crmValido(String(dados.crm)) : false;
+      const crnInvalido = campoExtra.crn ? !crnValido(String(dados.crn)) : false;
+
+      showCampoInvalido({
+         email: emailInvalido,
+         cpf: cpfInvalido,
+         data: dataInvalida,
+         senha: senhaInvalida,
+         crm: crmInvalido,
+         crn: crnInvalido,
+      });
+
+      if (emailInvalido || cpfInvalido || dataInvalida || senhaInvalida || crmInvalido || crnInvalido) {
+         return;
+      }
+
+      cadastrarUsuario(dados);
+   }
+
    return (
       <div>
-         <form className="formulario-cadastro-usuarios" onSubmit={validaFormulario}>
+         <form
+            className="formulario-cadastro-usuarios" onSubmit={validaFormulario}
+            style={{
+               padding: "10px", border: "2px solid black", borderRadius: "10px", backgroundColor: "white",
+               display:"flex", flexDirection:"column", gap:"20px"
+            }}
+         >
             <div className="cadastro-usuarios-campo-basico">
                <label>Nome:</label>
                <input name="nome" type="text" placeholder="Insira o nome" required/>
             </div>
             <div className="cadastro-usuarios-campo-basico">
                <label>E-mail:</label>
-               <input name="email" type="email" placeholder="Insira o email" required/>
+               <input name="email" type="email" placeholder="Insira o email" required />
+               { campoInvalido.email && <span style={{ color: "red" }}>E-mail inválido.</span>}
             </div>
             <div className="cadastro-usuarios-campo-basico">
                <label>CPF:</label>
-               <input name="cpf" type="text" placeholder="000.000.000-00" maxLength={14} value={cpf} onChange={(event) => modificaCPF(event.target.value)} required/>
+               <input name="cpf" type="text" placeholder="000.000.000-00" maxLength={14} value={cpf} onChange={(event) => modificaCPF(event.target.value)} required />
+               { campoInvalido.cpf && <span style={{ color: "red" }}>Cpf inválido.</span>}
             </div>
             <div className="cadastro-usuarios-campo-basico">
                <label>Data de Nascimento:</label>
                <input name="dataNascimento" type="date" required />
+               { campoInvalido.data && <span style={{ color: "red" }}>Data inválida.</span>}
             </div>
             <div className="cadastro-usuarios-campo-basico">
                <label>Senha:</label>
-               <input name="senha" type="text" required />
+               <input name="senha" type="password" required />
+               { campoInvalido.senha && <span style={{ color: "red" }}>Senhas diferentes.</span>}
             </div>
             <div className="cadastro-usuarios-campo-basico">
                <label>Repetir senha:</label>
-               <input name="repetirSenha" type="text" required />
+               <input name="repetirSenha" type="password" required />
             </div>
-            <div className="container-atribuir-perfil">
+
+            <div className="container-atribuir-perfil" style={{ display:"flex", flexDirection:"row", gap:"15px" }}>
                <div className="campo-atribuir-perfil">
-                  <input type="radio" name="perfil" id="paciente" value={1} required/>
+                  <input type="radio" name="perfil" id="paciente" value={1} onChange={() => {showCampoExtra({ crm:false, crn:false,})}} required/>
                   <label htmlFor="paciente">paciente</label>
                </div>
                <div className="campo-atribuir-perfil">
-                  <input type="radio" name="perfil" id="cuidador" value={2} />
+                  <input type="radio" name="perfil" id="cuidador" value={2} onChange={() => {showCampoExtra({ crm:false, crn:false,})}} />
                   <label htmlFor="cuidador">cuidador</label>
                </div>
                <div className="campo-atribuir-perfil">
-                  <input type="radio" name="perfil" id="medico" value={3} />
+                  <input type="radio" name="perfil" id="medico" value={3} onChange={() => {showCampoExtra({ crm:true, crn:false,})}} />
                   <label htmlFor="medico">médico</label>
                </div>
                <div className="campo-atribuir-perfil">
-                  <input type="radio" name="perfil" id="nutricionista" value={4} />
+                  <input type="radio" name="perfil" id="nutricionista" value={4} onChange={() => {showCampoExtra({ crm:false, crn:true,})}} />
                   <label htmlFor="nutricionista">nutricionista</label>
                </div>
             </div>
 
-            <div className="container-botoes-formulario-atribuir-perfil">
-               <button
+            {campoExtra.crm &&
+               <div className="campo-extra-perfil">
+                  <label htmlFor="crm">CRM:</label>
+                  <input type="text" name="crm" id="crm" required/>
+                  { campoInvalido.crm && <span style={{ color: "red" }}>CRM inválida.</span>}
+               </div>
+            }
+            {campoExtra.crn &&
+               <div className="campo-extra-perfil">
+                  <label htmlFor="crn">CRN:</label>
+                  <input type="text" name="crn" id="crn" required/>
+                  { campoInvalido.crn && <span style={{ color: "red" }}>CRN inválida.</span>}
+               </div>
+            }
+
+            <div
+               className="container-botoes-formulario-atribuir-perfil"
+               style={{ display:"flex", justifyContent:"center", gap:"30px" }}
+            >
+               <Button
                   type="button"
                   onClick={() => router.back()}
                >
-                  Cancelar</button>
-               <button
+                  Cancelar</Button>
+               <Button
                   type="submit"
                >
                   Cadastrar
-               </button>
+               </Button>
             </div>
          </form>
       </div>
    )
 }
 
-
-function validaFormulario(event: React.FormEvent<HTMLFormElement>) {
-   event.preventDefault();
-
-   const formData = new FormData(event.currentTarget);
-   const dados = Object.fromEntries(formData.entries())
-
-   const cpf = limparCPF(String(dados.cpf))
-
-   if (!cpfValido(cpf)) {
-      console.log("c");
-      return;
-   }
-
-   if (!emailValido(String(dados.email))) {
-      console.log("b");
-      return;
-   }
-
-   if (dados.senha !== dados.repetirSenha) {
-      console.log("a");
-      return;
-   }
-
-
-   cadastrarUsuario(dados);
-}
 // TO DO: Testar o fetch e saber se ele fica aqui ou se deve ser movido.
 async function cadastrarUsuario(dados: Record<string, FormDataEntryValue>) {
+
+   console.log("sucesso");
+
    await fetch("/api/v1/usuarios", {
       method: "POST",
       headers: {
--- vida-plena/app/admin/usuarios/page.tsx
+++ vida-plena/app/admin/usuarios/page.tsx
@@ -4,36 +4,235 @@ import Link from "next/link";
 import { useState } from "react";
 import { formatarCPF, limparCPF } from "@/app/lib/Formatters";
 
+import Button from "@/app/components/Button";
+import { UsuarioResponse } from "@/app/types/auth";
+import Image from "next/image";
+
 export default function Usuarios() {
    const [busca, setBusca] = useState("");
-   // TO DO: Enviar o cpfLimpo para o backend e receber uma lista de cpfs para mostrar na tela
-   // ao clicar o admin pode "acessar" aquele perfil de usuário e gerenciar (inativar, ativa, excluir).
-   function buscarUsuarios(cpf: string) {
+   const [usuarioEncontrado, setUsuarioEncontrado] = useState(false);
+   const [usuario, setUsuario] = useState<UsuarioResponse | null>(null);
+   const [resultados, setResultados] = useState<string[]>([]);
 
-      const cpfFormatado = formatarCPF(cpf);
+   // Objetos para teste
+   // QUAIS SÃO AS CLASSES DO BANCO DE DADOS?
+   // COMO PERFIL DEVE SER ENVIADO? STRING? INT?
+   const CpfsParaTeste = [
+      "12345678910",
+      "11111111111",
+      "12354312355",
+      "34565487623",
+      "98077857355",
+      "44566674580",
+      "78932455512",
+      "65423312344",
+   ];
+
+   function pesquisarUsuarios(cpf: string) {
+
       const cpfLimpo = limparCPF(cpf);
 
-      setBusca(cpfFormatado);
-      console.log("Buscando no lado do cliente:", cpfFormatado);
+      setBusca(cpfLimpo);
 
-      console.log("Buscando no Banco de dados:", cpfLimpo);
+      const CpfsBuscados = CpfsParaTeste.filter((cpf) =>
+         cpf.startsWith(cpfLimpo)
+      );
+      setResultados(CpfsBuscados);
+   }
+
+   function buscarUsuario(cpf: string) {
+
+      setBusca(cpf);
+      setResultados([]);
+
+      // setUsuario() Envia para o backend aqui e recebe os dados do usuário. Chamar pelo ID?
+      let usuarioTeste: UsuarioResponse | null = null;
+      switch (cpf){
+         case "12345678910":
+            usuarioTeste = {
+               id: "1",
+               nome: "João da Silva",
+               email: "joao@email.com",
+               cpf: "12345678910",
+               senha: 123,
+               telefone: "75999999999",
+               status: "ATIVO",
+               tipos: ["MEDICO"],
+               dataNascimento: "1985-06-15",
+               dataCriacao: "2026-08-01",
+            };
+            break;
+
+         case "11111111111":
+            usuarioTeste = {
+               id: "2",
+               nome: "Maria Oliveira",
+               email: "maria@email.com",
+               cpf: "11111111111",
+               telefone: "75988888888",
+               senha: 123,
+               status: "INATIVO",
+               tipos: ["PACIENTE"],
+               dataNascimento: "1992-03-22",
+               dataCriacao: "2026-08-02",
+            };
+            break;
+
+         case "12354312355":
+            usuarioTeste = {
+               id: "3",
+               nome: "Carlos Santos",
+               email: "carlos@email.com",
+               cpf: "12354312355",
+               telefone: "75977777777",
+               senha: 123,
+               status: "ATIVO",
+               tipos: ["RECEPCIONISTA"],
+               dataNascimento: "1978-11-10",
+               dataCriacao: "2026-08-03",
+            };
+            break;
+
+         case "34565487623":
+            usuarioTeste = {
+               id: "4",
+               nome: "Ana Costa",
+               email: "ana@email.com",
+               cpf: "34565487623",
+               telefone: "75966666666",
+               senha: 123,
+               status: "INATIVO",
+               tipos: ["PACIENTE"],
+               dataNascimento: "1988-07-05",
+               dataCriacao: "2026-08-04",
+            };
+            break;
+
+         default:
+            console.log("Usuario não encontrado!");
+            setUsuario(null);
+            break;
+      }
+
+      setUsuario(usuarioTeste);
+      setUsuarioEncontrado(true);
    }
 
    return (
-      <div>
-         <Link href={"/admin/usuarios/novo"}>
-            Cadastrar novo usuario
-         </Link>
+      <div className="gerenciar-usuarios-container" style={{ border:"1px solid black", padding:"10px", borderRadius:"10px", display:"flex", flexDirection:"column", gap:"20px" }}>
+         <Button>
+            <Link href={"/admin/usuarios/novo"}>
+               Cadastrar novo usuario
+            </Link>
+         </Button>
          <div className="container-busca-usuarios">
             <label>Digite o cpf de um usuário:</label>
             <input
                type="search"
-               placeholder="Digite o CPF do usuário..."
-               value={busca}
-               onChange={(event) => buscarUsuarios(event.target.value)}
+               placeholder="Digite aqui..."
+               value={formatarCPF(busca)}
+               onChange={(event) => pesquisarUsuarios(event.target.value)}
                maxLength={14}
+               style={{ border:"1px solid black", borderRadius:"4px", marginLeft:"4px" }}
             />
+            {busca.length > 0 && (
+               <div className="lista-resultados">
+                  {resultados.map((busca) => (
+                     <Button onClick={() => buscarUsuario(busca)} key={busca}>
+                        {formatarCPF(busca)}
+                     </Button>
+                  ))}
+               </div>
+            )}
          </div>
+
+
+         { usuarioEncontrado &&
+            <div
+               className="container-card-usuario"
+               style={{
+                  display: "flex", flexDirection: "row",
+                  gap: "20px", border: "1.5px solid black",
+                  borderRadius: "10px", padding: "10px",
+                  backgroundColor:"", color:"white"
+               }}
+            >
+               <div
+                  className="profile-image-portrait"
+                  style={{
+                     width:"100px", height:"200px",
+                     overflow:"hidden"
+                  }}
+               >
+                  <Image
+                     src={usuario?.fotoPerfil || "/images/DefaultUserImage.png"}
+                     alt="User not found"
+                     width={200}
+                     height={200}
+                     style={{
+                        objectFit:"cover"
+                     }}
+                  />
+               </div>
+               <div className="card-usuario-info-container">
+                  <div
+                     className="user-profile-info-container"
+                     style={{ display:"flex", justifyContent:"space-between", gap:"50px" }}
+                  >
+                     <div className="Principal user-info">
+                        <div className="campo-container">
+                           <label>Nome: {usuario?.nome}</label>
+                        </div>
+                        <div className="campo-container">
+                           <label>E-mail: {usuario?.email}</label>
+                        </div>
+                        <div className="campo-container">
+                           <label>CPF: {usuario?.cpf}</label>
+                        </div>
+                        <div className="campo-container">
+                           <label>Data de Nascimento: {usuario?.dataNascimento}</label>
+                        </div>
+                     </div>
+                     <div className="Secondary user-info">
+                        <div className="campo-container">
+                           <label>Perfil: </label>
+                        </div>
+                        {usuario?.tipos.includes("MEDICO") &&
+                           <div className="campo-container">
+                              <label>CRM: usuario?.crm</label>
+                           </div>
+                        }
+
+                        {/* IMPORTANTE: não existe usuario nutricionista
+                           usuario?.tipos.includes("NUTRICIONISTA") &&
+                           <div className="campo-container">
+                              <label>CRN: usuario?.crn</label>
+                           </div>
+                        */}
+
+                        <div className="campo-container">
+                           <label>Senha: {usuario?.senha}</label>
+                        </div>
+
+                        <div className="campo-container">
+                           <label>Telefone: {usuario?.telefone}</label>
+                        </div>
+                        <div className="campo-container">
+                           <label>Status: {usuario?.status}</label>
+                        </div>
+                     </div>
+                  </div>
+                  <div className="Admin-buttons" style={{ display:"flex", flexDirection:"row", gap:"20px" }}>
+                     {usuario?.status === "ATIVO" &&
+                        <Button>Inativar</Button>
+                     }
+                     {usuario?.status === "INATIVO" &&
+                        <Button>Ativar</Button>
+                     }
+                     <Button variant="danger" >Excluir</Button>
+                  </div>
+               </div>
+            </div>}
 
       </div>
    )
