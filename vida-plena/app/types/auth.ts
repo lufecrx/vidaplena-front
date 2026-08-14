3757de6c577ef6d3517d64aad06cbbbba889c46f
Igor B Sousa <ibarbosadsousa@gmail.com>
2026-08-13 21:08:42 -0300

    tipos de usuarios

--- vida-plena/app/types/auth.ts
+++ vida-plena/app/types/auth.ts
@@ -15,12 +15,34 @@ export interface UsuarioResponse {
   nome: string
   email: string
   cpf: string
+  senha: number
   telefone?: string
   fotoPerfil?: string
   status: 'ATIVO' | 'INATIVO'
   tipos: TipoUsuario[]
+  dataNascimento: string
   dataCriacao: string
 }
+
+/* O número define qual tipo de usuario. Começando de 1, 2, 3, e assim por diante.
+export interface Paciente extends UsuarioResponse {
+   perfil: number
+}
+
+export interface Cuidador extends UsuarioResponse {
+   perfil: number
+}
+
+export interface Medico extends UsuarioResponse {
+   perfil: number
+   crm: string
+}
+
+export interface Nutricionista extends UsuarioResponse {
+   perfil: number
+   crn: string
+}
+*/
 
 export interface LoginRequest {
   email: string
