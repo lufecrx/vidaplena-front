00fb8383725a588b8246c6906cd3b9d83fb952ae
Igor B Sousa <ibarbosadsousa@gmail.com>
2026-08-17 10:21:42 -0300

    talvez não seja o melhor a se fazer

--- vida-plena/app/types/auth.ts
+++ vida-plena/app/types/auth.ts
@@ -15,30 +15,37 @@ export interface UsuarioResponse {
   nome: string
   email: string
   cpf: string
-  senha: number
   telefone?: string
   fotoPerfil?: string
   status: 'ATIVO' | 'INATIVO'
   tipos: TipoUsuario[]
-  dataNascimento: string
   dataCriacao: string
 }
 
 /* O número define qual tipo de usuario. Começando de 1, 2, 3, e assim por diante.
-export interface Paciente extends UsuarioResponse {
+export interface Usuario {
+   nome: string
+   cpf: string
+   email: string
+   senha: string
+   telefone: string
+   dataNascimento: string
+}
+
+export interface Paciente extends Usuario {
    perfil: number
 }
 
-export interface Cuidador extends UsuarioResponse {
+export interface Cuidador extends Usuario {
    perfil: number
 }
 
-export interface Medico extends UsuarioResponse {
+export interface Medico extends Usuario {
    perfil: number
    crm: string
 }
 
-export interface Nutricionista extends UsuarioResponse {
+export interface Nutricionista extends Usuario {
    perfil: number
    crn: string
 }
