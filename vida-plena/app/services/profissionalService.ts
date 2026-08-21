de9be1b6b581986be5352bf8b50c47fe0bde64e5
Natycunham <nataliacunhamarques@gmail.com>
2026-08-21 12:44:55 -0300

    feat: cria servicos de profissionais

---
+++ vida-plena/app/services/profissionalService.ts
@@ -0,0 +1,71 @@
+import { api } from '../../api';
+export type Especialidade = 
+  | 'CLINICO_GERAL'
+  | 'CARDIOLOGIA'
+  | 'DERMATOLOGIA'
+  | 'GINECOLOGIA'
+  | 'PEDIATRIA'
+  | 'PSICOLOGIA'
+  | 'NUTRICAO'
+  | 'FISIOTERAPIA'
+  | 'ENFERMAGEM'
+  | 'ORTOPEDIA'
+  | 'OTOLOGIA'
+  | 'ODONTOLOGIA';
+
+export interface FiltrosProfissional{
+    especialidade?:Especialidade;
+    data?: string;
+    clinicaId?: string;
+    nome?: string;
+}
+
+export interface RetornaProfissional {
+   id: string;
+   usuarioId: string;
+   nome?: string;
+   registroConselho: string;
+   especialidade: Especialidade;
+   clinicaId: string;
+}
+
+export interface CadastrarProfissional {
+   usuarioId: string;
+   registroConselho: string;
+   especialidade: Especialidade;
+   clinicaId: string;
+}
+
+export type AtualizarProfissional = Partial<CadastrarProfissional>;
+
+export const profissionalService = {
+    async listarProfissionais(filtros?:FiltrosProfissional) : Promise<RetornaProfissional[]>{
+      await new Promise(resolve => setTimeout(resolve, 1000)); //so pra vizualizar
+        const response = await api.get('/api/profissionais', { params: filtros });
+        let dados = response.data;
+        if (filtros && filtros.especialidade) {
+            dados = dados.filter(
+                (profissional: RetornaProfissional) => profissional.especialidade === filtros.especialidade
+            );
+        }
+
+        return dados;
+    },
+    async obterProfissional(id: string): Promise<RetornaProfissional> {
+     const response = await api.get(`/api/profissionais/${id}`);
+     return response.data;
+    },
+    async cadastrarProfissional(dados: CadastrarProfissional) {
+      const { data } = await api.post('/api/profissionais', dados);
+      return data;
+   },
+   async atualizarProfissional(id: string, dados: AtualizarProfissional) {
+      const response = await api.put(`/api/profissionais/${id}`, dados);
+      return response.data;
+   },
+   async excluirProfissional(id: string) {
+      const result = await api.delete(`/api/profissionais/${id}`);
+      return result.data;
+   }
+};
+
