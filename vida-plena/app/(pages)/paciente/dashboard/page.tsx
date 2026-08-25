9360c06ce9d5bec67ba35298f40d5e70e07032b3
Igor B Sousa <ibarbosadsousa@gmail.com>
2026-08-25 11:04:21 -0300

    alterando caminho das páginas

---
+++ vida-plena/app/(pages)/paciente/dashboard/page.tsx
@@ -0,0 +1,18 @@
+
+
+export default function PacienteDashboard() {
+   return (
+      <div>
+         {/*<aside>
+            <ul>
+               <li><li/>
+               <li><li/>
+               <li><li/>
+            </ul>
+            </aside>*/}
+         <div className="tela-principal">
+
+         </div>
+      </div>
+   )
+}
